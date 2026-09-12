0db337a6509d9155a3c903ec4b314ce3eb1c6248
Sougata Mondal <sougatacodewhiz@gmail.com>
2026-09-12 12:29:54 +0530

    on site chnages

--- backend/scripts/sms.sh
+++ backend/scripts/sms.sh
@@ -1,4 +1,4 @@
 curl -X POST -u $1:$2 \
   -H "Content-Type: application/json" \
-  -d '{ "textMessage": { "text": "This is an Automated Message from Team Lead for SIH SMS Testing" }, "phoneNumbers": ["+918159073507", "+916289645167", "+916290289863", "+918337049905", "+91747 7349159", "+917439407308"] }' \
+  -d '{ "textMessage": { "text": "This is an Automated Message from Team Lead for SIH SMS Testing" }, "phoneNumbers": ["+916289645167"] }' \
   http://$3:8080/message
